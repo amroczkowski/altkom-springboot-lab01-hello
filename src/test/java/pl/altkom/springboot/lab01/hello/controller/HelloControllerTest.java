86c8cf44377f63b34e13663ecbfc5a844389a39c
Artur Mroczkowski <a.mroczkowski@intive.com>
2025-11-29 15:49:00 +0100

    Spring Boot 3.5.8 Upgrade

--- src/test/java/pl/altkom/springboot/lab01/hello/controller/HelloControllerTest.java
+++ src/test/java/pl/altkom/springboot/lab01/hello/controller/HelloControllerTest.java
@@ -2,13 +2,15 @@ package pl.altkom.springboot.lab01.hello.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@WebMvcTest(controllers = HelloController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class HelloControllerTest {
 
     @Autowired
